726f173503303ecfa31b1457be69c6204d4a0e5d
Aakash <aakash877848@gmail.com>
2026-09-18 12:36:00 +0530

    fix corrupted axios.js baseURL

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: sed -i '' "s|baseURL: 'https://meridian-founder.onrender.com/api'|baseURL: '/api'|" frontend/src/api/axios.js,
+  baseURL: '/api',
   withCredentials: true, // send/receive httpOnly auth cookies
 });
 
