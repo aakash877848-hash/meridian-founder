e213b73cdf34e4aa9da52bebd1c77674c98a5daa
Aakash <aakash877848@gmail.com>
2026-09-18 10:42:47 +0530

    point frontend to live backend

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: 'https://meridian-founder.onrender.com/api',
   withCredentials: true, // send/receive httpOnly auth cookies
 });
 
